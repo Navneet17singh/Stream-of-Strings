1f1b528e1543bd58f06e5b6606548cdd1b0fdb9f
Navneet17singh <117436999+Navneet17singh@users.noreply.github.com>
2022-11-04 15:31:23 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,16 @@
+package com.codebode.project
+
+public classPrime{
+	psvm
+	int n=9;
+	boolean prime=true;
+	for(int i=2; i<n; i++){
+		if(n%2==0)
+		{
+			prime=false;
+			break;
+		}
+	}
+	System.out.println(prime);
+		}
+	}
