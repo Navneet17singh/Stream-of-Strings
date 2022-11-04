0c5fa8fa003faac372808cf35a6744bd80c3a508
Navneet17singh <117436999+Navneet17singh@users.noreply.github.com>
2022-11-04 15:51:01 +0530

    Add files via upload

---
+++ Stream.java
@@ -0,0 +1,8 @@
+
+public class{
+Public static void main(String[] args{
+	List number = Array.asList(2,3,4,5);
+	List square = number.stream().map(x->x*).collect(Collectors.toList());
+	System.out.println(Square);
+}
+}
